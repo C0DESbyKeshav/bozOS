7011d0c53071998b24f64f1bde5ab70d4f225e05
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-06-22 17:40:31 +0530

    Extracted initial context states

--- contexts/session.tsx
+++ contexts/session.tsx
@@ -2,8 +2,11 @@ import useSessionContextState from "hooks/useSessionContextState";
 import type { FC, PropsWithChildren } from "react";
 import { createContext } from "react";
 import type { SessionContextState } from "types/contexts/session";
+import { initialSessionContextState } from "utils/initialContextStates";
 
-const SessionContext = createContext<SessionContextState>({});
+const SessionContext = createContext<SessionContextState>(
+  initialSessionContextState
+);
 
 export const SessionProvider: FC<PropsWithChildren> = ({ children }) => (
   <SessionContext.Provider value={useSessionContextState()}>
