1b7c349c64d41efdf2ae3d5607a16c21ac413483
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-06-28 01:57:19 +0530

    Embed default props in pages components & Store theme name in session context

--- components/pages/StyledApp.tsx
+++ components/pages/StyledApp.tsx
@@ -1,15 +1,17 @@
 import { SessionConsumer } from "contexts/session";
-import type { FC } from "react";
+import type { FC, PropsWithChildren } from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
-import type { StyledAppProps } from "types/components/pages/StyledApp";
+import themes from "styles/themes";
 
-const StyledApp: FC<StyledAppProps> = ({ children, currentTheme }) => (
+const StyledApp: FC<PropsWithChildren> = ({ children }) => (
   <>
     <GlobalStyle />
     <SessionConsumer>
-      {({ theme = currentTheme }) => (
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      {({ themeName }) => (
+        <ThemeProvider theme={themes[themeName] || themes.defaultTheme}>
+          {children}
+        </ThemeProvider>
       )}
     </SessionConsumer>
   </>
--- types/components/pages/StyledApp.ts
+++
@@ -1,6 +0,0 @@
-import type { DefaultTheme } from "styled-components";
-
-export type StyledAppProps = {
-  children: React.ReactNode;
-  currentTheme: DefaultTheme;
-};
