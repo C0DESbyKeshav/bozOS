27e6d186493aab63c33f4ea708996ab213ee19e6
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-06-30 04:13:11 +0530

    Set max-width for Taskbar entries

--- styles/components/system/Taskbar/StyledTaskbarEntry.ts
+++ styles/components/system/Taskbar/StyledTaskbarEntry.ts
@@ -4,9 +4,9 @@ const StyledTaskbarEntry = styled.li`
   background-color: #7555bf;
   display: flex;
   height: 100%;
+  max-width: ${({ theme }) => theme.sizes.taskbar.entry.maxWidth};
   place-content: center;
   place-items: center;
-  width: ${({ theme }) => theme.sizes.taskbar.entry.width};
 `;
 
 export default StyledTaskbarEntry;
