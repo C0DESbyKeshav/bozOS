2463bc29cbe31eb9b4620d621b06cca0661676b8
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-06-29 16:52:23 +0530

    Refactored Wallpaper element/type

--- hooks/useWallpaper.ts
+++ hooks/useWallpaper.ts
@@ -1,12 +1,14 @@
 import { useContext, useEffect } from "react";
 import { ThemeContext } from "styled-components";
 
-const useWallpaper = (desktopRef: React.RefObject<HTMLElement>): void => {
+const useWallpaper = (
+  desktopRef: React.RefObject<HTMLElement | null>
+): void => {
   const theme = useContext(ThemeContext);
   const wallpaper = theme?.wallpaper;
 
   useEffect(() => {
-    wallpaper?.(desktopRef);
+    wallpaper?.(desktopRef.current);
   }, [desktopRef, wallpaper]);
 };
 
