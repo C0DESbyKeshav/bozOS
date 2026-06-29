0ca6caf63960cfb075e8375d97f39fc8fbb2ab57
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-06-30 02:16:20 +0530

    Added dateTime to time element

--- components/system/Taskbar/Clock.tsx
+++ components/system/Taskbar/Clock.tsx
@@ -1,5 +1,7 @@
 import StyledClock from "styles/components/system/Taskbar/StyledClock";
 
-const Clock: React.FC = () => <StyledClock>01:46:25 AM</StyledClock>;
+const Clock: React.FC = () => (
+  <StyledClock dateTime="23:17:22">11:17:22 PM</StyledClock>
+);
 
 export default Clock;
