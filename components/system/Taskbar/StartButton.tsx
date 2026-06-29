3d77992bc81732011178656276ef07e53bacaa86
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-06-30 03:59:39 +0530

    Added Start tooltip to start button

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -2,7 +2,9 @@ import type { JSX } from "react/jsx-runtime";
 import StyledStartButton from "styles/components/system/Taskbar/StyledStartButton";
 
 const StartButton = (): JSX.Element => (
-  <StyledStartButton type="button">K</StyledStartButton>
+  <StyledStartButton type="button" title="Start">
+    K
+  </StyledStartButton>
 );
 
 export default StartButton;
--- styles/components/system/Taskbar/StyledClock.ts
+++ styles/components/system/Taskbar/StyledClock.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledClock = styled.time`
   background-color: #c5d327;
   display: flex;
+  font-size: ${({ theme }) => theme.fonts.clock.size};
   height: 100%;
   place-content: center;
   place-items: center;
