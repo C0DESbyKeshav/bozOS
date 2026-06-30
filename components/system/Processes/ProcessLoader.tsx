ecdb91cb8256eba2f3f5a2200ecf9b6117d428f7
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-06-30 19:09:25 +0530

    Switched ProcessReducer to renderProcesses function

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,15 +1,16 @@
 import RenderProcess from "components/system/Processes/RenderProcess";
 import { ProcessConsumer } from "contexts/process";
 import type { JSX } from "react/jsx-runtime";
-import type { Process } from "types/contexts/process";
+import type { Processes } from "types/contexts/process";
 
-const ProcessesReducer = ([id, process]: [string, Process]) => (
-  <RenderProcess key={id} {...process} />
-);
+const renderProcesses = (processes: Processes) =>
+  Object.entries(processes).map(([id, process]) => (
+    <RenderProcess key={id} {...process} />
+  ));
 
 const ProcessLoader = (): JSX.Element => (
   <ProcessConsumer>
-    {({ processes }) => Object.entries(processes).map(ProcessesReducer)}
+    {({ processes }) => renderProcesses(processes)}
   </ProcessConsumer>
 );
 
