6d234f895e4e2c11d3019dec0059fbc2de4b40ae
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-02 00:46:44 +0530

    Added start button icon and reorganized theme

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -1,8 +1,12 @@
+import { faWindows } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { JSX } from "react/jsx-runtime";
 import StyledStartButton from "styles/components/system/Taskbar/StyledStartButton";
 
 const StartButton = (): JSX.Element => (
-  <StyledStartButton title="Start">K</StyledStartButton>
+  <StyledStartButton title="Start">
+    <FontAwesomeIcon icon={faWindows} />
+  </StyledStartButton>
 );
 
 export default StartButton;
