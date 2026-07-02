63fb666fc8400d28d012872f8785cbc3c468bee7
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-02 19:09:59 +0530

    File System Update

---
+++ hooks/useFileSystemContextState.ts
@@ -0,0 +1,26 @@
+import * as BrowserFS from "browserfs";
+import type { FSModule } from "browserfs/dist/node/core/FS";
+import { useEffect, useState } from "react";
+import type { FileSystemContextState } from "types/contexts/fileSystem";
+
+const useFileSystemContextState = (): FileSystemContextState => {
+  const [fs, setFs] = useState<FSModule | null>(null);
+
+  useEffect(() => {
+    BrowserFS.install(window);
+
+    BrowserFS.configure(
+      {
+        fs: "IndexedDB",
+        options: {}
+      },
+      () => {
+        setFs(BrowserFS.BFSRequire("fs"));
+      }
+    );
+  }, [setFs]);
+
+  return { fs };
+};
+
+export default useFileSystemContextState;
