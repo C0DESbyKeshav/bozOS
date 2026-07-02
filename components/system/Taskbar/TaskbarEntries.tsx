47982cbda1554d6938f03f75303f995810d4f47d
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-03 03:27:30 +0530

    Added taskbar entries and icon/title to process

--- components/system/Taskbar/TaskbarEntries.tsx
+++ components/system/Taskbar/TaskbarEntries.tsx
@@ -1,10 +1,17 @@
 import TaskbarEntry from "components/system/Taskbar/TaskbarEntry";
+import { ProcessConsumer } from "contexts/process";
 import type { JSX } from "react/jsx-runtime";
 import StyledTaskbarEntries from "styles/components/system/Taskbar/StyledTaskbarEntries";
 
 const TaskbarEntries = (): JSX.Element => (
   <StyledTaskbarEntries>
-    <TaskbarEntry />
+    <ProcessConsumer>
+      {({ processes }) =>
+        Object.entries(processes).map(([id, { icon, title }]) => (
+          <TaskbarEntry key={id} icon={icon} title={title} />
+        ))
+      }
+    </ProcessConsumer>
   </StyledTaskbarEntries>
 );
 
