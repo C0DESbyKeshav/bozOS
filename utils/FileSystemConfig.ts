00b53735923598cb1a86d54aa08f278721edd48d
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-03 00:42:50 +0530

    Simplified file system context

---
+++ utils/FileSystemConfig.ts
@@ -0,0 +1,7 @@
+import type { FileSystemConfiguration } from "browserfs";
+
+const FileSystemConfig: FileSystemConfiguration = {
+  fs: "IndexedDB",
+  options: {}
+};
+export default FileSystemConfig;
