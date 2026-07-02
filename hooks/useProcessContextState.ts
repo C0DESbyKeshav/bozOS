ed8dfcdc07645e2d034fea535c1f637efcfbd158
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-03 03:31:00 +0530

    Removed example HelloWorld

--- hooks/useProcessContextState.ts
+++ hooks/useProcessContextState.ts
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import type { ProcessContextState, Processes } from "types/contexts/process";
-import { HelloWorld } from "utils/processDirectory";
 
 const useProcessContextState = (): ProcessContextState => {
-  const [processes] = useState<Processes>({ HelloWorld });
+  const [processes] = useState<Processes>({});
 
   return { processes };
 };
