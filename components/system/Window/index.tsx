be2f1c1474ee6fd69ae0916a5eebcf6566e3c063
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-05 04:23:47 +0530

    Created titlebar component

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -1,7 +1,8 @@
 import type { ProcessComponentProps } from "components/system/Processes/RenderProcess";
+import Titlebar from "components/system/Window/Titlebar";
 import { useProcesses } from "contexts/process";
 import type { PropsWithChildren } from "react";
-import StyledWindow from "styles/components/system/StyledWindow";
+import StyledWindow from "styles/components/system/Window/StyledWindow";
 
 const Window = ({ children, id }: PropsWithChildren<ProcessComponentProps>) => {
   const {
@@ -10,7 +11,12 @@ const Window = ({ children, id }: PropsWithChildren<ProcessComponentProps>) => {
     }
   } = useProcesses();
 
-  return <StyledWindow minimized={minimized}>{children}</StyledWindow>;
+  return (
+    <StyledWindow minimized={minimized}>
+      <Titlebar id={id} />
+      {children}
+    </StyledWindow>
+  );
 };
 
 export default Window;
