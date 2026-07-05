39cf0cb8338d8c7465e7e0906729771e44df7f43
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-05 05:39:01 +0530

    Added resize hook

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -1,21 +1,32 @@
 import type { ProcessComponentProps } from "components/system/Processes/RenderProcess";
 import Titlebar from "components/system/Window/Titlebar";
 import { useProcesses } from "contexts/process";
-import type { PropsWithChildren } from "react";
+import useResizable from "hooks/useResizable";
+import { type PropsWithChildren } from "react";
+import { Rnd } from "react-rnd";
 import StyledWindow from "styles/components/system/Window/StyledWindow";
+import rndDefaults from "utils/rndDefaults";
 
 const Window = ({ children, id }: PropsWithChildren<ProcessComponentProps>) => {
   const {
     processes: {
-      [id]: { minimized }
+      [id]: { maximized, minimized }
     }
   } = useProcesses();
+  const { height, width, updateSize } = useResizable(maximized);
 
   return (
-    <StyledWindow minimized={minimized}>
-      <Titlebar id={id} />
-      {children}
-    </StyledWindow>
+    <Rnd
+      enableResizing={!maximized}
+      size={{ height, width }}
+      onResizeStop={updateSize}
+      {...rndDefaults}
+    >
+      <StyledWindow minimized={minimized}>
+        <Titlebar id={id} />
+        {children}
+      </StyledWindow>
+    </Rnd>
   );
 };
 
