61b6492be61c9d279a56c2c2b56523355308b10b
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-07-07 01:48:48 +0530

    Changed theme key to name

--- styles/themes.ts
+++ styles/themes.ts
@@ -1,7 +1,7 @@
 import type { DefaultTheme } from "styled-components";
 import defaultTheme from "styles/defaultTheme";
 
-type Themes = { [key: string]: DefaultTheme };
+type Themes = { [name: string]: DefaultTheme };
 const themes: Themes = { defaultTheme };
 
 export default themes;
