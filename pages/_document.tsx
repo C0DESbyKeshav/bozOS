ee8d69c4003550f346061caf4d066b0b58046422
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-08-01 20:20:13 +0530

    Added html lang back to `_document`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,26 @@
 import type { DocumentContext, DocumentInitialProps } from "next/document";
-import Document from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import type { JSX } from "react/jsx-runtime";
 import withServerStyleSheet from "styles/withStyledComponents";
+import { DEFAULT_LOCALE } from "utils/constants";
 
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
     return withServerStyleSheet(ctx);
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang={DEFAULT_LOCALE}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
