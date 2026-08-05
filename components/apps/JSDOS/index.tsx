3efa5a84b67da4192302f8a93be99d7a24720bcf
C0DESbyKeshav <keshavmandal2503@gmail.com>
2026-08-05 21:12:47 +0530

    Added JS-DOS app

---
+++ components/apps/JSDOS/index.tsx
@@ -0,0 +1,22 @@
+import StyledJSDOS from "components/apps/JSDOS/StyledJSDOS";
+import useJSDOS from "components/apps/JSDOS/useJSDOS";
+import type { ProcessComponentProps } from "components/system/Processes/RenderProcess";
+// eslint-disable-next-line import/no-cycle
+import { useProcesses } from "contexts/process";
+import type { JSX } from "react";
+import { useRef } from "react";
+
+const JSDOS = ({ id }: ProcessComponentProps): JSX.Element => {
+  const {
+    processes: {
+      [id]: { url = "" }
+    }
+  } = useProcesses();
+  const screenRef = useRef<HTMLDivElement | null>(null);
+
+  useJSDOS(id, url, screenRef);
+
+  return <StyledJSDOS ref={screenRef} />;
+};
+
+export default JSDOS;
